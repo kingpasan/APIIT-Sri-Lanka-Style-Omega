e436459f4602cf12fdfbcf325933f12476e26a99
Pasan Maduranga Semage <pmsemage@gmail.com>
2017-09-18 15:28:02 +0530

    Project begin

---
+++ app/src/main/java/com/pasansemage/pasanmadurangasemagecb006067/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.pasansemage.pasanmadurangasemagecb006067;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
